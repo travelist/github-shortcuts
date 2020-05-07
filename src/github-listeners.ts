8a7fdbee3cd9956bae3c7766f116d26e77ae9397
Komei Shimamura <komei.t.f@gmail.com>
2020-05-08 08:42:52 +0900

    Support pull request page

--- src/github-listeners.ts
+++ src/github-listeners.ts
@@ -82,7 +82,7 @@ const currentPage = (): GithubPage | null => {
     const paths = window.location.pathname.split('/')
 
     if (paths.length >= 5 && paths[3].endsWith('issues')) return GithubPage.Issue
-    if (paths.length >= 5 && paths[3].endsWith('pulls')) return GithubPage.PullRequest
+    if (paths.length >= 5 && paths[3].endsWith('pull')) return GithubPage.PullRequest
 
     if (paths.length >= 4 && paths[3].endsWith('issues')) return GithubPage.RepositoryIssueList
     if (paths.length >= 2 && paths[1].endsWith('issues')) return GithubPage.UserIssues
